c044cdb073ecdc1ff623d2b163ce2184ed7ec534
nicklink415 <nbl2@rice.edu>
2015-02-25 17:14:07 -0600

    Stashing changes to index retrieval

--- 2Dplot/getBestStoredIndices.m
+++ 2Dplot/getBestStoredIndices.m
@@ -3,6 +3,10 @@ function [index1, index2] = getBestStoredIndices(theta1, theta2, disc)
 index1 = round(theta1*disc/(2*pi));
 index2 = round(theta2*disc/(2*pi));
 
+% dt = 2*pi/disc;
+% index1 = round(theta1+pi-dt)/dt;
+% index2 = round(theta2+pi-dt)/dt;
+
 return
 
 
